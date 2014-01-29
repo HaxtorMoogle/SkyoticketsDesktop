87c6325d8a3a6ef8c559d9a4a9a761b970a4daaf
Skyost <skyost@mail.com>
2014-01-29 18:08:27 +0100

    v0.3.1.

--- src/fr/skyost/tickets/desktop/frames/OutputFrame.java
+++ src/fr/skyost/tickets/desktop/frames/OutputFrame.java
@@ -1,21 +1,29 @@
 package fr.skyost.tickets.desktop.frames;
 
+import java.awt.Container;
 import java.awt.Font;
 
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 
 public class OutputFrame extends JFrame {
 
 	private static final long serialVersionUID = -4083882619877335524L;
-	private final String lineSeparator = System.getProperty("line.separator");
+	private final Container pane = getContentPane();
+	private final String lineSeparator = System.lineSeparator();
 	private JTextArea txtOutput;
 	
 	private static OutputFrame instance;
 	
 	public OutputFrame() {
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		instance = this;
 		setAlwaysOnTop(true);
 		setType(Type.UTILITY);
@@ -26,7 +34,19 @@ public class OutputFrame extends JFrame {
 		txtOutput.setText("Output :");
 		txtOutput.setEditable(false);
 		txtOutput.setFont(new Font("Lucida Console", Font.PLAIN, 13));
-		getContentPane().add(new JScrollPane(txtOutput, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS));
+		final JScrollPane scrollPane = new JScrollPane(txtOutput, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+		pane.add(scrollPane);
+		
+		final JButton btnClearOutput = new JButton("Clear output");
+		btnClearOutput.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(final ActionEvent event) {
+				txtOutput.setText(null);
+			}
+			
+		});
+		scrollPane.setColumnHeaderView(btnClearOutput);
 	}
 	
 	public void addText(final String text) {
